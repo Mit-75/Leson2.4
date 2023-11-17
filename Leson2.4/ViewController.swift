dd9d051d0a5dc94c4de513d07a83b795263e849b
Dmitry Parkhomenko <oku_74@mail.ru>
2023-11-17 07:22:37 +0500

    Initial Commit

---
+++ Leson2.4/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Leson2.4
+//
+//  Created by Dmitry Parhomenko on 17.11.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
